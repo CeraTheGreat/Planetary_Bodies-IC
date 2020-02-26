70c48fde664ebb6d390d3e1aee252b545dac995f
Grumpy_Engineer <lpburgard3@gmail.com>
2020-02-26 12:47:20 -0600

    Added controller object interactivity.

---
+++ Assets/Scripts/ControllerGrabObject.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class ControllerGrabObject : MonoBehaviour
+{
+    public SteamVR_Input_Sources handType;
+    public SteamVR_Behaviour_Pose controllerPose;
+    public SteamVR_Action_Boolean grabAction;
+
+    private GameObject collidingObject;
+    private GameObject objectInHand;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (grabAction.GetLastStateDown(handType))
+        {
+            //print("grab action?");
+            if (collidingObject)
+            {
+                GrabObject();
+                //print("Grabed?");
+            }
+        }
+        if (grabAction.GetLastStateUp(handType))
+        {
+            if (objectInHand)
+            {
+                ReleaseObject();
+            }
+        }
+    }
+
+    private void SetCollidingObject(Collider col)
+    {
+        if(collidingObject || !col.GetComponent<Rigidbody>())
+        {
+            return;
+        }
+
+        collidingObject = col.gameObject;
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        SetCollidingObject(other);
+        print("Enter!");
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+        SetCollidingObject(other);
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (!collidingObject)
+        {
+            return ;
+        }
+
+        collidingObject = null;
+        print("Exit!");
+    }
+
+    private void GrabObject()
+    {
+        //switching objects
+        objectInHand = collidingObject;
+        collidingObject = null;
+
+        var joint = addFixedJoint();
+        joint.connectedBody = objectInHand.GetComponent<Rigidbody>();
+    }
+
+    private FixedJoint addFixedJoint()
+    {
+        FixedJoint fx = gameObject.AddComponent<FixedJoint>();
+        fx.breakForce = 20000;
+        fx.breakTorque = 20000;
+        return fx;
+    }
+
+    private void ReleaseObject()
+    {
+        if (GetComponent<FixedJoint>())
+        {
+            GetComponent<FixedJoint>().connectedBody = null;
+            Destroy(GetComponent<FixedJoint>());
+
+            objectInHand.GetComponent<Rigidbody>().velocity = controllerPose.GetVelocity();
+            objectInHand.GetComponent<Rigidbody>().angularVelocity = controllerPose.GetAngularVelocity();
+        }
+
+        objectInHand = null;
+    }
+}
