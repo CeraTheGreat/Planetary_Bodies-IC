b3a2c9da8fc51e6e17ce280d30332cbf617a1d16
Grumpy_Engineer <lpburgard3@gmail.com>
2020-03-01 16:22:52 -0600

    Commit because I don't know why things are fucked up

---
+++ Assets/OrbitMapScripts/JupiterOrbit.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JupiterOrbit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Vector3 sun = new Vector3(0.0f, 2.0f, 0.0f);
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(sun, Vector3.up, 30 * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/UranusRotate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UranusRotate : MonoBehaviour
+{
+    public GameObject uranus;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        uranus.transform.Rotate(0, 1, 0);
+    }
+}
