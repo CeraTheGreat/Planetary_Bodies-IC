6ce2e57af8585a49d19bc63733efc14a55f9d035
Grumpy_Engineer <lpburgard3@gmail.com>
2020-03-01 19:06:30 -0600

    Created solar system Scene Fixed orbit paths Fixed rotation Fixed lighting

---
+++ Assets/Scripts/Orbiter.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbiter : MonoBehaviour
+{
+
+    public GameObject parentBody;
+    public Vector3 rotationAxis;
+    public float orbialPeriod;
+    public float rotationalPeriod;
+    private GameObject satelite;
+    private Vector3 orbitAxis;
+    private float deltaOrbitalAngle;
+    private float deltaRotationalAngle;
+    private float speedup;
+
+ 
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        satelite = this.gameObject;
+        orbitAxis = new Vector3(0f, 1f, 0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        speedup = parentBody.GetComponent<PlanetSpeedup>().getSpeedup();
+
+        deltaRotationalAngle = (360f / rotationalPeriod) * speedup;
+        deltaOrbitalAngle = (360f / orbialPeriod) * speedup;
+        
+        satelite.transform.RotateAround(parentBody.transform.position, orbitAxis, Time.deltaTime * deltaOrbitalAngle);
+        satelite.transform.Rotate(0f, Time.deltaTime * deltaRotationalAngle, 0f);
+    }
+}
---
+++ Assets/Scripts/PlanetSpeedup.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class PlanetSpeedup : MonoBehaviour
+{
+    public SteamVR_Input_Sources handType;
+    public SteamVR_Action_Boolean speedupAction;
+    private float speedMultiplyer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speedMultiplyer = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (speedupAction.GetState(handType))
+        {
+            print("Speed!");
+            speedMultiplyer = 3;
+        }
+        else
+        {
+            speedMultiplyer = 1;
+        }
+    }
+
+    public float getSpeedup()
+    {
+        return speedMultiplyer;
+    }
+}
